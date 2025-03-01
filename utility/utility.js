843e7a61f86cc2a8c877de98aa694cc7bde9eabf
Faysal <fr.abir.ahmed.faysal@gmail.com>
2025-03-01 12:16:44 +0600

    link

--- utility/utility.js
+++ utility/utility.js
@@ -2,8 +2,6 @@ document.getElementById('theme-btn').addEventListener('click',function(event){
     const baseColor=document.getElementById('rootColor');
 
     const random =Math.floor(Math.random()*10);
-    
-    
     const colorList=[
         "bg-blue-500","bg-red-500","bg-green-500","bg-yellow-500","bg-purple-500","bg-pink-500","bg-teal-500","bg-orange-500","bg-indigo-500","bg-gray-500"
     ];
@@ -16,3 +14,9 @@ document.getElementById('theme-btn').addEventListener('click',function(event){
     }
 })
 
+
+
+a=document.getElementById('discover-Btn').addEventListener('click',function(){
+window.location.href='./q&a.html';
+
+})
