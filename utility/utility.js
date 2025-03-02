1b2ca5ce8411770c75cfa26a49b5204391fbb3fe
Faysal <fr.abir.ahmed.faysal@gmail.com>
2025-03-02 10:45:21 +0600

    project ready

--- utility/utility.js
+++ utility/utility.js
@@ -1,4 +1,6 @@
-document.getElementById('theme-btn').addEventListener('click',function(event){  
+
+document.getElementById('theme-btn').addEventListener
+('click',function(event){  
     const baseColor=document.getElementById('rootColor');
 
     const random =Math.floor(Math.random()*10);
@@ -14,8 +16,16 @@ document.getElementById('theme-btn').addEventListener('click',function(event){
     }
 })
 
-            // link discover btn
 
+            // Clear Button
+            document.getElementById('clear-btn').addEventListener('click' ,function(){
+                let parent = document.getElementById("terry-parent");
+                parent.replaceChildren();
+                })
+
+
+
+            // link discover btn
 a=document.getElementById('discover-Btn').addEventListener('click',function(){
 window.location.href='./q&a.html';
 })
@@ -41,48 +51,45 @@ window.location.href='./q&a.html';
             const currentYear=new Date().getFullYear();
             document.getElementById('year').innerText=currentYear;
 
+            
 
 const featureList=document.querySelectorAll('.feature');
-
 for(core of featureList){
     core.addEventListener('click',function(event){
 
-            //include disabled attribute& alert
+            //include disabled attribute& alert showing
             event.target.setAttribute("disabled", "true");
          alert('Board Updated Successfully');
-         
-
-
-
-
-
-
-
-
-
-
          
             //total process increase
         const totalProcess=parseInt(document.getElementById('total-process').innerText);
         if(event.target.classList.contains("feature")){
             const sum=totalProcess+1;
             document.getElementById('total-process').innerText=sum;
-        }
+        };
 
+                // current time
+        const currentTime = new Date().toLocaleTimeString();
 
+                //heading innerText 
+        const mainParent=event.target.parentNode.parentNode.parentNode;
+        const heading=mainParent.querySelector('h2').innerText;
+        
+                // parentElement
+        const activityTerry=document.getElementById('terry-parent');
 
-
-
-
-
-
-
-
-
-
-
-
-
+                // create new Element
+        const newDiv=document.createElement('div');
+        newDiv.classList.add('bg-gray-200','rounded-xl','overflow-hidden','p-2','remover-child');
+        
+        
+        
+        newDiv.innerHTML=`
+        <p>You have completed the task ${heading} at ${currentTime}</p>
+        `;
+                // append child
+        activityTerry.appendChild(newDiv);
+        
 
         // limit of Six task
         const taskLimit=parseInt(document.getElementById('task-limit').innerText);
@@ -91,30 +98,12 @@ for(core of featureList){
             const sum=taskLimit-1;
             document.getElementById('task-limit').innerText=sum;
              if(sum===0){
-                alert('complete all')
+                alert('Congrats!!! You have completed all the current tasks')
                 return;
              }
          }
-        
-        
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-         
     })
 }
+
+
 
